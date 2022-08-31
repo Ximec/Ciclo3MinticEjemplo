7ef2710109970ddd77e5c59dda6c6c40e525ed10
ximec <ximenacampol@gmail.com>
2022-08-30 22:40:40 -0500

    se crea servicio controller empresa

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -1,13 +1,29 @@
 package com.UdeA.Ciclo3;
 
+import com.UdeA.Ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola ciclo 3";
+	}
+
+	@GetMapping("/test")
+	public String test(){
+		Empresa emp = new Empresa("Meter SAS","calle 6", "3232323","12345");
+		return emp.getDireccion();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+
 	}
 
 }
