fc8d1311fc43f87bbaaa32a0ab497982654197f9
ximec <ximenacampol@gmail.com>
2022-09-02 17:48:24 -0500

    Se crea database ciclo3 y tablas empleado y empresa

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -1,5 +1,6 @@
 package com.UdeA.Ciclo3;
 
+import com.UdeA.Ciclo3.modelos.Empleado;
 import com.UdeA.Ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +16,16 @@ public class Ciclo3Application {
 		return "Hola ciclo 3";
 	}
 
-	@GetMapping("/test")
-	public String test(){
+	@GetMapping("/testempr")
+	public String testempr(){
 		Empresa emp = new Empresa("Meter SAS","calle 6", "3232323","12345");
 		return emp.getDireccion();
+	}
+
+	@GetMapping("/testempl")
+	public String testempl(){
+		Empleado empl = new Empleado("Jose","correo.correo@algo", "MeterSas","Soporte Técnico");
+		return empl.getRolEmpleado();
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
@@ -1,4 +1,70 @@
 package com.UdeA.Ciclo3.modelos;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+
+    //Atributos de la clase Empleado
+    private int id;
+    private String nombre;
+    private String correo;
+    private String empresaPertenece;
+    private String rolEmpleado;
+
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String correo, String empresaPertenece, String rolEmpleado) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresaPertenece = empresaPertenece;
+        this.rolEmpleado = rolEmpleado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEmpresaPertenece() {
+        return empresaPertenece;
+    }
+
+    public void setEmpresaPertenece(String empresaPertenece) {
+        this.empresaPertenece = empresaPertenece;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
 }
